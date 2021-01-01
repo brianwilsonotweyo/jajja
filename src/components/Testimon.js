a67b02bac883cd861db2f0a5bd9e08617f49fad6
brian otweyo <brianotweyo@gmail.com>
2021-01-01 08:43:05 +0300

    testimonies

--- src/components/Testimon.js
+++ src/components/Testimon.js
@@ -24,9 +24,31 @@ function Testimon() {
                     </div>
                     <div class="testimonials-content">
                       <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
+                        Good day everybody, My name is Alicia Leonard I am out
+                        here to spread this good news to the entire world on how
+                        I got my ex husband back. It all started when my husband
+                        cheated on me with another woman unknowing to him that
+                        the woman is a wizard, the woman cast a spell on my
+                        husband which made my husband change his feelings
+                        towards me and the kids and broke our 6 years marriage.
+                        I was confused and stressed because of the pains of
+                        being a single mother, so I called a friend and
+                        explained my marital challenges to her, she instructed
+                        and directed me to contact this great powerful spell
+                        caster called Jajja Walu, The main reason why she
+                        instructed me to contact Dr Raypower was because in
+                        recent times she have read some testimonies on the
+                        internet which some people has written about the
+                        powerful spell caster Dr Raypower and i was so pleased
+                        and i decided to seek for assistance from him which he
+                        did a perfect job by casting a spell on my husband and
+                        utilize the powers of the other woman which made him to
+                        come back to me and beg for forgiveness. I will not stop
+                        publishing his name on the net because of the good work
+                        he is doing. To everyone with marital problems, divorce
+                        issues, lost lover or any relationship related issues,
+                        you can contact Jajja Walu the powerful urgent spell
+                        caster
                       </p>
                     </div>
                     <div class="testimonial-author-box">
@@ -35,7 +57,7 @@ function Testimon() {
                         alt=""
                         class="testimonial-avatar"
                       />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
+                      <h5 class="testimonial-author">Alicia Leonard</h5>
                     </div>
                   </div>
                   <div class="col-sm-12 col-md-6">
@@ -44,9 +66,24 @@ function Testimon() {
                     </div>
                     <div class="testimonials-content">
                       <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
+                        My name is Kristen Van Weyenberg. I never knew that
+                        there will be a time where my husband can no longer look
+                        into my eyes and tells me he loves me. I suffered heart
+                        break when my husband left home to be with another lady
+                        in her apartment without caring for me and our lovely
+                        daughter. I had to do 3 shifts every day to pay my bills
+                        and also take care of my daughter. Last month, I came
+                        across an article online saying how Lord Zakuza as been
+                        so helpful to a lots of people in the restoration to
+                        their broken love lives. I contacted him through this
+                        Email: Lordzakuza7@gmail.com and he told me that he will
+                        restore my marriage within 48 hours with his magical
+                        powers and I believed in his words that my husband will
+                        come back and “wow”, within 48 hours after I contacted
+                        Jajja Walu, my husband came back begging saying he
+                        didn’t know what came over him that he’s sorry and
+                        promised never to abandon I and our daughter again I am
+                        so grateful to him for restoring my marriage.
                       </p>
                     </div>
                     <div class="testimonial-author-box">
@@ -55,49 +92,7 @@ function Testimon() {
                         alt=""
                         class="testimonial-avatar"
                       />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-sm-12 col-md-6">
-                    <div class="testimonial-ico">
-                      <span class="ion-ios-quote"></span>
-                    </div>
-                    <div class="testimonials-content">
-                      <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
-                      </p>
-                    </div>
-                    <div class="testimonial-author-box">
-                      <img
-                        src="img/mini-testimonial-1.jpg"
-                        alt=""
-                        class="testimonial-avatar"
-                      />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
-                    </div>
-                  </div>
-                  <div class="col-sm-12 col-md-6">
-                    <div class="testimonial-ico">
-                      <span class="ion-ios-quote"></span>
-                    </div>
-                    <div class="testimonials-content">
-                      <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
-                      </p>
-                    </div>
-                    <div class="testimonial-author-box">
-                      <img
-                        src="img/mini-testimonial-1.jpg"
-                        alt=""
-                        class="testimonial-avatar"
-                      />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
+                      <h5 class="testimonial-author">Kristen Van Weyenberg</h5>
                     </div>
                   </div>
                 </div>
@@ -108,9 +103,32 @@ function Testimon() {
                     </div>
                     <div class="testimonials-content">
                       <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
+                        EmilySarah 16 days ago Get your husband back from the
+                        other woman After Divorce or Break Up..Even If Your
+                        Situation Seems Hopeless.. I just got my husband back
+                        through the help of genuine love spell caster dr unity.
+                        My name is Emily Sarah am from Tx,USA. My husband left
+                        me for another woman, This was just 3 years of our
+                        marriage. The most painful thing is that I was pregnant
+                        with our second baby. I wanted him back. I did
+                        everything within my reach to bring him back but all was
+                        in vain, I wanted him back so badly because of the love
+                        I had for him, I begged him with everything, I made
+                        promises but he refused. I explained my problem to my
+                        friend and she suggested that I should rather contact a
+                        spell caster that could help me cast a spell to bring
+                        him back , I had no choice than to try it. I messaged
+                        the spell caster called dr unity, and he assured me
+                        there was no problem and that everything will be okay
+                        before 11 hours. He cast the spell and surprisingly 11
+                        hours later my husband called me. I was so surprised, I
+                        answered the call and all he said was that he was so
+                        sorry for everything that had happened He wanted me to
+                        return to him. He also said he loved me so much. I was
+                        so happy and went to him that was how we started living
+                        together happily again.thanks to dr unity . if you are
+                        here and your Lover is turning you down, or your husband
+                        moved to another woman, do not cry anymore,
                       </p>
                     </div>
                     <div class="testimonial-author-box">
@@ -119,7 +137,7 @@ function Testimon() {
                         alt=""
                         class="testimonial-avatar"
                       />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
+                      <h5 class="testimonial-author">Emily Sarah</h5>
                     </div>
                   </div>
                   <div class="col-sm-12 col-md-6">
@@ -128,9 +146,22 @@ function Testimon() {
                     </div>
                     <div class="testimonials-content">
                       <p class="testimonial-text">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Omnis, cupiditate ea nam praesentium debitis hic
-                        ber quibusdam voluptatibus officia expedita corpori.
+                        After 3 Years in a relationship with my Boyfriend, my
+                        Boyfriend started going out with other girls and showing
+                        me cold love, on several occasions he threatens to
+                        broke-up with me if I dare question him about his affair
+                        with other girls, I was totally devastated and confused
+                        until an old friend of mine told me about a spell caster
+                        on the internet DR GREAT who help people with their
+                        relationship and marriage problem by the powers of love
+                        Spells, At first I doubted If such thing ever exists but
+                        decided to give it a try, When I contact him, he told me
+                        everything I needed to do and I did and he helped me
+                        cast a Love Spell and within 28Hours my boyfriend came
+                        back to me and started apologizing, now he has stopped
+                        going out with girls and he is with me for good and for
+                        real. Contact this Great spell Caster for your
+                        relationship or marriage problem.
                       </p>
                     </div>
                     <div class="testimonial-author-box">
@@ -139,7 +170,72 @@ function Testimon() {
                         alt=""
                         class="testimonial-avatar"
                       />
-                      <h5 class="testimonial-author">Albert & Erika</h5>
+                      <h5 class="testimonial-author">Astoria khrine</h5>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-sm-12 col-md-6">
+                    <div class="testimonial-ico">
+                      <span class="ion-ios-quote"></span>
+                    </div>
+                    <div class="testimonials-content">
+                      <p class="testimonial-text">
+                        I feel so blessed again in my marriage after Jajja Walu
+                        brought back my husband that separated with me for 5
+                        months. I searched for help everywhere but nothing
+                        worked out not until I met Jajja Walu who I contacted
+                        via WhatsApp I explained my situation
+                        to Jajja Walu and He promised my husband will get back
+                        to me within 24, as far as my heart still beats for my
+                        husband. I believed in Jajja Walu and she prepared a
+                        spell for me and my husband came back exactly when
+                        Jajja Walu mentions. He pleaded and said he needs me
+                        back and now we are living happily again for the past 9
+                        months.
+                      </p>
+                    </div>
+                    <div class="testimonial-author-box">
+                      <img
+                        src="img/mini-testimonial-1.jpg"
+                        alt=""
+                        class="testimonial-avatar"
+                      />
+                      <h5 class="testimonial-author">Jessica Wayen</h5>
+                    </div>
+                  </div>
+                  <div class="col-sm-12 col-md-6">
+                    <div class="testimonial-ico">
+                      <span class="ion-ios-quote"></span>
+                    </div>
+                    <div class="testimonials-content">
+                      <p class="testimonial-text">
+                        This is my testimony about the good work of Jajja Walu
+                        who help me.... am sorry for putting this on net but i
+                        will have to, by this world best spell caster that
+                        brought back my husband who left me out for past 3
+                        years, i eventually met this man on a blog site posting
+                        by one of is client for help, i explained everything to
+                        him and he told me about a spell caster that he had
+                        heard about and he gave me an email address to write to
+                        the spell caster to tell him my problems. In just 2
+                        days, my husband was back to me. I just want to say
+                        thank you to this truthful and sincere spell caster, sir
+                        all you told me have come to pass and thank you Dr Jumba
+                        . Please I want to tell everyone who is looking for a
+                        solution to their problems, I advise you to kindly
+                        consult Dr Jumba , he is real,he is powerful and
+                        whatever the spell caster tells is what will happen,
+                        because all what the spell caster told me came to pass.
+                      </p>
+                    </div>
+                    <div class="testimonial-author-box">
+                      <img
+                        src="img/mini-testimonial-1.jpg"
+                        alt=""
+                        class="testimonial-avatar"
+                      />
+                      <h5 class="testimonial-author">Bella George</h5>
                     </div>
                   </div>
                 </div>
